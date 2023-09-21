5e6ae06f365ddc13133e24244ee21bc733a81ba5
srimsd <144526972+srimsd@users.noreply.github.com>
2023-09-20 20:07:58 -0700

    Add files via upload

---
+++ unit 2 (1).py
@@ -0,0 +1,30 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please deposit a positive amount.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Insufficient funds or invalid withdrawal amount.")
+
+    def display_balance(self):
+        print(f"Account balance for {self.__account_holder_name}: ${self.__account_balance}")
+
+if __name__ == "__main__":
+    account = BankAccount("123456789", "gokul", 1000.0)
+
+    account.display_balance()
+    account.deposit(500.0)
+    account.withdraw(200.0)
+    account.display_balance
